770865a23a48c2db7443b0836cb92b7dab078245
Alyssom Falkenberg Martins <alyssommartins@gmail.com>
2018-06-25 14:33:46 -0300

    Login
    Login com Google através do Firebase.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { key } from './firebase.credencial';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(key.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+    NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [AngularFirestoreModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/firebase.credencial.ts
@@ -0,0 +1,10 @@
+export const key = {
+    firebase: {
+        apiKey: "AIzaSyCarOwXXVz8P_0oGcj83m0wzb0cepFwQ3A",
+        authDomain: "cincos-7e4a1.firebaseapp.com",
+        databaseURL: "https://cincos-7e4a1.firebaseio.com",
+        projectId: "cincos-7e4a1",
+        storageBucket: "cincos-7e4a1.appspot.com",
+        messagingSenderId: "396042909317"
+    }
+}

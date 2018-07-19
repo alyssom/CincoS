3f9c17890ec4f1538611ac4881966a39ab28d756
Alyssom Falkenberg Martins <alyssommartins@gmail.com>
2018-07-19 10:30:36 -0300

    PopUp

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -74,25 +74,38 @@ export class HomeComponent implements OnInit {
     
   }
   salvar(){
-    if(this.funcionario != undefined){
+    const swal = require('sweetalert2')
 
-    let nome = this.funcionario.nome;
-    let pontosDesperdicio = this.funcionario.pontosDesperdicio;
-    let pontosDisciplina = this.funcionario.pontosDisciplina;
-    let pontosOrganizacao = this.funcionario.pontosOrganizacao;
-    let pontosLimpeza = this.funcionario.pontosLimpeza;
-    let pontosPadrao = this.funcionario.pontosPadrao;
-    let id = this.funcionario.id;
-    let cargo = this.funcionario.cargo;
-    let email = this.funcionario.email;
-    let sala = this.funcionario.sala;
-    let idade = this.funcionario.idade;
-    let mediaFuncionario = (pontosDesperdicio + pontosDisciplina + pontosLimpeza + pontosOrganizacao + pontosPadrao) / 5;
+      if(this.funcionario != undefined){
+        let nome = this.funcionario.nome;
+        let pontosDesperdicio = this.funcionario.pontosDesperdicio;
+        let pontosDisciplina = this.funcionario.pontosDisciplina;
+        let pontosOrganizacao = this.funcionario.pontosOrganizacao;
+        let pontosLimpeza = this.funcionario.pontosLimpeza;
+        let pontosPadrao = this.funcionario.pontosPadrao;
+        let id = this.funcionario.id;
+        let cargo = this.funcionario.cargo;
+        let email = this.funcionario.email;
+        let sala = this.funcionario.sala;
+        let idade = this.funcionario.idade;
+        let mediaFuncionario = (pontosDesperdicio + pontosDisciplina + pontosLimpeza + pontosOrganizacao + pontosPadrao) / 5;
 
-    let item = {id, nome, pontosDesperdicio, pontosDisciplina, pontosOrganizacao, pontosLimpeza, pontosPadrao, mediaFuncionario, cargo, email, sala, idade}; 
-    this.db.collection('funcionarios', ref => ref.where('id', '==', this.funcionario.id)).doc(this.funcionario.id).set(item)
-
-    }    
+        let item = {id, nome, pontosDesperdicio, pontosDisciplina, pontosOrganizacao, pontosLimpeza, pontosPadrao, mediaFuncionario, cargo, email, sala, idade}; 
+        this.db.collection('funcionarios', ref => ref.where('id', '==', this.funcionario.id)).doc(this.funcionario.id).set(item)
+        swal({
+          position: 'center',
+          type: 'success',
+          title: 'Nota alterada com sucesso!',
+          showConfirmButton: false,
+          timer: 2000
+        })
+      }else{
+        swal(
+          'Oops...',
+          'Acho que você não tem nada para ser salvo.',
+          'error'
+        )
+      }    
   }
 
 }
